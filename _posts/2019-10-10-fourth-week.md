bf84bd25ed779d9dd9c561a86111e6e743b767cc
Albert Liu <albertymliu@gmail.com>
2019-10-15 17:36:53 -0400

    added fourth week info

--- _posts/2019-10-10-fourth-week.md
+++ _posts/2019-10-10-fourth-week.md
@@ -1,9 +1,21 @@
 ---
 title: "Wearables Week 4: Programming a Simple Watch"
-published: false
 ---
-I'm going to try to program a simple alarm clock. I'm going to need to handle
-quite a few things, so this is a longer term project.
+I'm going to write a simple library for handling different input types on a single
+pin.
 
 ### Power Saving
+I went on the [H2OhNo! project guide][h2ohno] and found some useful tips for saving
+power:
+
+1. Turn off analog-to-digital conversion (ADC). This saves around 230uA.
+2. Go to sleep when possible. This can save entire amps.
+
+[h2ohno]: https://learn.sparkfun.com/tutorials/h2ohno
+
+### Phase 1 Project Proposal
+My project will be a simple stuffed animal that can change colors when you squeeze
+it. Squeezing it twice should turn it off/on, and squeezing it once should change
+the color that's being displayed.
+
 
--- src/hello-world/hello-world.ino
+++ src/hello-world/hello-world.ino
@@ -1,45 +1,33 @@
+#include <avr/sleep.h>
+
+int statusPin = 0;
+int inputPin = 1;
+int outputPin = 2;
+
+enum class InputPin {
+  Hours,
+  Minutes
+};
+
+boolean read_pin() {
+}
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0, OUTPUT); // Alarm
-  pinMode(1, OUTPUT); // Status
-  pinMode(2, INPUT); // StartStop
-  pinMode(3, INPUT); // Hours
-  pinMode(4, INPUT); // Minutes
+  pinMode(statusPin, OUTPUT);
+  pinMode(inputPin, INPUT);
+  pinMode(outputPin, OUTPUT);
+  digitalWrite(statusPin, HIGH);
 
   // From the H2OhNo tutorial
   ADCSRA &= ~(1<<ADEN); //Disable Analog-to-Digital Converter, saves ~230uA
 }
 
-enum class InputPin {
-  Hours,
-  Minutes,
-  StartStop
-};
-
-enum class OutputPin {
-  Sound,
-  Status
-};
-
-boolean read_pin(InputPin &pin) {
-  if (analogRead(2) > 128) {
-    pin = StartStop;
-    return true;
-  } else if (analogRead(3) > 128) {
-    pin = Hours;
-    return true;
-  } else if (analogRead(4) > 128) {
-    pin = Minutes;
-    return true;
-  } else return false;
+void loop() {
+  short b = analogRead(inputPin);
+  analogWrite(outputPin, b);
 }
 
-short hours_ = 0;
-short minutes_ = 0;
-
-void loop() {
-  InputPin pin;
-  if (read_pin(pin)) { // Logic for configuring alarm
-  } else {
-  }
+boolean read_button() {
+  return true;
 }
