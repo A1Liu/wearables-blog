fd3031698d7a5736659ebfe4cb6dbfc5bf63c062
Albert Liu <albertymliu@gmail.com>
2019-11-10 03:41:03 -0500

    added intervals function

---
+++ src/intervals/intervals.ino
@@ -0,0 +1,26 @@
+#include "pins.h"
+
+void setup() {
+  pinMode(0, PinMode::Output);
+  pinMode(1, PinMode::Output);
+  pinMode(2, PinMode::Output);
+  pinMode(3, PinMode::Output);
+  pinMode(4, PinMode::Output);
+}
+
+
+void loop() {
+  static int counter = 23;
+  delay(200);
+
+  if (counter % 2 == 0) flipPin(0);
+  if (counter % 3 == 0) flipPin(1);
+  if (counter % 4 == 0) flipPin(2);
+  if (counter % 6 == 0) flipPin(3);
+  if (counter % 12 == 0) flipPin(4);
+
+  if (counter == 0) {
+    counter = 23;
+  }
+}
+
